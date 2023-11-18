8c9b02c20c5e6e73d77b916db15e0c89b5cb4c2c
Ulugbek <ulugbekiut@gmail.com>
2023-11-18 12:08:35 -0800

    IsIsomorphic words

--- PascalTraingle.js
+++ PascalTraingle.js
@@ -22,3 +22,26 @@ var removeElement = function(nums, val) {
     }
     return nums.length;
 }
+
+function isIsomorphic(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const mapS = {};
+    const mapT = {};
+
+    for (let i = 0; i < s.length; i++) {
+        const charS = s[i];
+        const charT = t[i];
+
+        if ((mapS[charS] && mapS[charS] !== charT) || (mapT[charT] && mapT[charT] !== charS)) {
+            return false;
+        }
+
+        mapS[charS] = charT;
+        mapT[charT] = charS;
+    }
+
+    return true;
+}
