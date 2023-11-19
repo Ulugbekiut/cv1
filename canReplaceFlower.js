3105cfe9da620fac21ac8c4a94cefa9832ca8b26
Ulugbek <ulugbekiut@gmail.com>
2023-11-18 21:35:15 -0800

    find pivot index

--- canReplaceFlower.js
+++ canReplaceFlower.js
@@ -25,3 +25,17 @@ function majorityElement(nums) {
 
     return candidate;
 }
+
+
+unction pivotIndex(nums) {
+    let totalSum = nums.reduce((a,b)=>(a+b),0)
+    let leftSum= 0;
+   for(let i=0; i<nums.length; i++){
+       if(leftSum === totalSum -leftSum-nums[i]){
+           return i
+       }
+       leftSum+=nums[i]
+   }
+   return -1
+   }
+   
