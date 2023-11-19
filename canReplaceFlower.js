ca99dabb8cce31562f18192835506f7b96178897
Ulugbek <ulugbekiut@gmail.com>
2023-11-19 10:35:29 -0800

    Binary tree levelOrder Traversal

--- canReplaceFlower.js
+++ canReplaceFlower.js
@@ -39,3 +39,25 @@ function pivotIndex(nums) {
    return -1
    }
    
+   function levelOrder(root) {
+    if (!root) return [];
+    
+    const result = [];
+    const queue = [root];
+
+    while (queue.length > 0) {
+        const level = [];
+        const levelSize = queue.length;
+
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift();
+            level.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        result.push(level);
+    }
+
+    return result;
+} //Binary tree levelOrder traversal . this question we use bfs and levelsize. Really good for visualize
