c16cf5f5ac4fef606690e92e06d61c0e40bb1a1c
Ulugbek <ulugbekiut@gmail.com>
2023-11-19 11:19:01 -0800

    clone graph

--- canReplaceFlower.js
+++ canReplaceFlower.js
@@ -60,4 +60,29 @@ function pivotIndex(nums) {
     }
 
     return result;
-} //Binary tree levelOrder traversal . this question we use bfs and levelsize. Really good for visualize
+} //Binary tree levelOrder traversal . this question we use bfs and levelsize. Really good for visualizevar cloneGraph = function(node) {
+    // Nodes we have already copied
+   // DFS approach
+var cloneGraph = function(node) {
+    // Nodes we have already copied
+    const visited = {};
+
+    // DFS function to copy graph
+    const dfs = (node) => {
+        if (!node) return node;
+        // If we have seen this node before, return it
+        if (visited[node.val]!=null) return visited[node.val];
+
+        // Create base for copied node
+        const root = new Node(node.val);
+        // Add this copied node to group of nodes we hav copied
+        visited[node.val] = root;
+
+        // Add copied neighbors to the current copied node
+        node.neighbors.forEach(n => root.neighbors.push(dfs(n)))
+        return root;
+    }
+
+    // Return new copied graph
+    return dfs(node);
+};
